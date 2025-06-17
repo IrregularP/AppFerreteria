d076863e2f412c3adaa7c9491eada063af3eb78c
ManuelMedina <manelik_123@hotmail.com>
2025-06-17 14:21:25 -0600

    Dependendencia Eliminada Codigos Nuevos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
